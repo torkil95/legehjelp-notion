e2a486e3466107f4d38c896e55bc60d1e29d5524
torkil95 <145580009+torkil95@users.noreply.github.com>
2023-12-15 18:00:23 +0100

    fiks error

--- site.config.ts
+++ site.config.ts
@@ -3,7 +3,6 @@ import { siteConfig } from './lib/site-config'
 export default siteConfig({
   // the site's root Notion page (required)
   rootNotionPageId: '1bd5f426796c44a1bd269ede1fcf692b',
-  rootNotionPageIdFraser: '295ce18bb9ac496585cdd036fc89088d',
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
