f266895028fcfd15c901c9e6f5eeee871b6b1bb7
torkil95 <145580009+torkil95@users.noreply.github.com>
2023-12-15 17:50:47 +0100

    fraser page

--- pages/fraser.tsx
+++ pages/fraser.tsx
@@ -1,9 +1,23 @@
-import React from 'react'
+import * as React from 'react'
 
-const fraser = () => {
-  return (
-    <div>fraser</div>
-  )
+import { NotionPage } from '@/components/NotionPageFraser'
+import { domain } from '@/lib/config'
+import { resolveNotionPage } from '@/lib/resolve-notion-page'
+
+export const getStaticProps = async () => {
+  try {
+    const props = await resolveNotionPage(domain)
+
+    return { props, revalidate: 10 }
+  } catch (err) {
+    console.error('page error', domain, err)
+
+    // we don't want to publish the error version of this page, so
+    // let next.js know explicitly that incremental SSG failed
+    throw err
+  }
 }
 
-export default fraser
+export default function NotionDomainPage(props) {
+  return <NotionPage {...props} />
+}
